e8fbf9e6b54cf4c52dd56542b7e96578bfa03c58
Joe <joetituspro@gmail.com>
2026-03-04 14:47:05 +0100

    Update asset version and enhance styling for improved UI consistency
    - Updated the version in `poststation-admin.asset.php` to reflect recent changes.
    - Added new Tailwind CSS classes for background and border colors to improve UI elements.
    - Modified focus and hover states for various components to use black and gray colors, enhancing accessibility and visual clarity.
    - Introduced global overrides for Tailwind indigo utilities to ensure consistent color usage across the plugin UI.

--- build/poststation-admin.asset.php
+++ build/poststation-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '606257047851c193b45d');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3b55bc029ca0b5379413');
